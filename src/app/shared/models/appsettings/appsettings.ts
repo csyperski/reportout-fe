1285e994299de77b0cd1237897ce3bb437e2fc8f
Charles Syperski <csyperski@gmail.com>
2022-05-17 18:02:32 +0000

    Updated top nav bar

--- src/app/shared/models/appsettings/appsettings.ts
+++ src/app/shared/models/appsettings/appsettings.ts
@@ -14,7 +14,7 @@ export class AppSettings {
     }
 
     public static get version(): string {
-        return "1.1.3";
+        return "1.2.0";
     }
 
     public static get production(): boolean {
